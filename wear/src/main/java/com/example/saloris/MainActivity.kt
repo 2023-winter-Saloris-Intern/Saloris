451af4cfd138dbb1cfe7eca85799aa8fa2e219e5
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-01-02 09:50:18 +0900

    commit android

---
+++ wear/src/main/java/com/example/saloris/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.saloris
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.saloris.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
