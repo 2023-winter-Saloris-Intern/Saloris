10a44a71d44396722334c2d39d6c456ef3c80faf
HeewonP825 <7hwon@naver.com>
2023-01-04 10:12:56 +0900

    editing ScanFragment & RecordFragment

--- mobile/src/main/java/com/example/saloris/Home/ScanFragment.kt
+++ mobile/src/main/java/com/example/saloris/Home/ScanFragment.kt
@@ -1,18 +1,38 @@
 package com.example.saloris.Home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.saloris.R
+import com.example.saloris.databinding.FragmentScanBinding
 
 class ScanFragment : Fragment() {
+    private var _binding: FragmentScanBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        _binding = FragmentScanBinding.inflate(inflater,container,false)
+
+//        binding.deviceScanBtn.setOnClickListener {
+//            loadFragment(ScanFragment())
+//        }
+
+        return binding.root
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        Log.d("clickTest", "click!->" + fragment.tag)
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
