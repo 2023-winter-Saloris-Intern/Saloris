c65c54753ad35c608701eefc0aed0070d15b4fff
song-na-min <minnasong0707@gmail.com>
2023-01-05 10:01:18 +0900

    wearOS to mobile message

---
+++ wear/src/main/java/com/example/saloris/NetWorking.kt
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.saloris
+
+import android.annotation.SuppressLint
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ScrollView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.wear.ambient.AmbientModeSupport
+import androidx.wear.ambient.AmbientModeSupport.AmbientCallback
+import com.google.android.gms.wearable.*
+import com.example.saloris.databinding.FragmentExerciseBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.cancel
+import java.nio.charset.StandardCharsets
+
+class NetWorking : WearableListenerService(),
+    DataClient.OnDataChangedListener,
+    MessageClient.OnMessageReceivedListener,
+    CapabilityClient.OnCapabilityChangedListener {
+    private var activityContext: Context? = null
+    private val TAG_MESSAGE_RECEIVED = "receive1"
+    private val APP_OPEN_WEARABLE_PAYLOAD_PATH = "/APP_OPEN_WEARABLE_PAYLOAD"
+    private var mobileDeviceConnected: Boolean = false
+
+    // Payload string items
+    private val wearableAppCheckPayloadReturnACK = "AppOpenWearableACK"
+
+    private val MESSAGE_ITEM_RECEIVED_PATH: String = "/message-item-received"
+
+
+    private var messageEvent: MessageEvent? = null
+    private var mobileNodeUri: String? = null
+    var steal : String = ""
+
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        activityContext = this
+        try {
+            Wearable.getDataClient(activityContext!!).addListener(this)
+            Wearable.getMessageClient(activityContext!!).addListener(this)
+            Wearable.getCapabilityClient(activityContext!!)
+                .addListener(this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        if(mobileDeviceConnected){
+            var S = intent.getStringExtra("heartRate")
+            if (S!!.isNotEmpty()) {
+                val nodeId: String = messageEvent?.sourceNodeId!!
+                // Set the data of the message to be the bytes of the Uri.
+                val payload: ByteArray =
+                    S.toByteArray()
+
+                // Send the rpc
+                // Instantiates clients without member variables, as clients are inexpensive to
+                // create. (They are cached and shared between GoogleApi instances.)
+                val sendMessageTask =
+                    Wearable.getMessageClient(activityContext!!)
+                        .sendMessage(nodeId, MESSAGE_ITEM_RECEIVED_PATH, payload)
+
+
+                sendMessageTask.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d("send1", "Message sent successfully")
+                    } else {
+                        Log.d("send1", "Message failed.")
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    activityContext,
+                    "Message content is empty. Please enter some message and proceed",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDataChanged(p0: DataEventBuffer) {
+    }
+
+    override fun onCapabilityChanged(p0: CapabilityInfo) {
+
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onMessageReceived(p0: MessageEvent) {
+        try {
+            Log.d(TAG_MESSAGE_RECEIVED, "onMessageReceived event received")
+            val s1 = String(p0.data, StandardCharsets.UTF_8)
+            val messageEventPath: String = p0.path
+
+            Log.d(
+                TAG_MESSAGE_RECEIVED,
+                "onMessageReceived() A message from watch was received:"
+                        + p0.requestId
+                        + " "
+                        + messageEventPath
+                        + " "
+                        + s1
+            )
+
+            //Send back a message back to the source node
+            //This acknowledges that the receiver activity is open
+            if (messageEventPath.isNotEmpty() && messageEventPath == APP_OPEN_WEARABLE_PAYLOAD_PATH) {
+                try {
+                    // Get the node id of the node that created the data item from the host portion of
+                    // the uri.
+                    val nodeId: String = p0.sourceNodeId.toString()
+                    steal = nodeId
+                    // Set the data of the message to be the bytes of the Uri.
+                    val returnPayloadAck = wearableAppCheckPayloadReturnACK
+                    val payload: ByteArray = returnPayloadAck.toByteArray()
+
+                    // Send the rpc
+                    // Instantiates clients without member variables, as clients are inexpensive to
+                    // create. (They are cached and shared between GoogleApi instances.)
+                    val sendMessageTask =
+                        Wearable.getMessageClient(activityContext!!)
+                            .sendMessage(nodeId, APP_OPEN_WEARABLE_PAYLOAD_PATH, payload)
+
+                    Log.d(
+                        TAG_MESSAGE_RECEIVED,
+                        "Acknowledgement message successfully with payload : $returnPayloadAck"
+                    )
+
+                    messageEvent = p0
+                    mobileNodeUri = p0.sourceNodeId
+
+                    sendMessageTask.addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Log.d(TAG_MESSAGE_RECEIVED, "meow")
+
+
+                            val sbTemp = StringBuilder()
+                            sbTemp.append("\nMobile device connected.")
+                            Log.d("receive1", " $sbTemp")
+
+
+                            mobileDeviceConnected = true
+                            Log.d("why!", mobileDeviceConnected.toString())
+                        } else {
+                            Log.d(TAG_MESSAGE_RECEIVED, "Message failed.")
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.d(
+                        TAG_MESSAGE_RECEIVED,
+                        "Handled in sending message back to the sending node"
+                    )
+                    e.printStackTrace()
+                }
+            }//emd of if
+            else if (messageEventPath.isNotEmpty() && messageEventPath == MESSAGE_ITEM_RECEIVED_PATH) {
+                try {
+
+                    val sbTemp = StringBuilder()
+                    sbTemp.append("\n")
+                    sbTemp.append(s1)
+                    sbTemp.append(" - (Received from mobile)")
+                    Log.d("receive1", " $sbTemp")
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } catch (e: Exception) {
+            Log.d(TAG_MESSAGE_RECEIVED, "Handled in onMessageReceived")
+            e.printStackTrace()
+        }
+    }
+
+
+}
