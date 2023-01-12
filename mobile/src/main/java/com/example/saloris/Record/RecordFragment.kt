96b3fa4acdc2ac95ade16143f8bfb3515442b3be
HeewonP825 <7hwon@naver.com>
2023-01-12 16:56:28 +0900

    calender collapse & expand

--- mobile/src/main/java/com/example/saloris/Record/RecordFragment.kt
+++ mobile/src/main/java/com/example/saloris/Record/RecordFragment.kt
@@ -1,13 +1,12 @@
 package com.example.saloris.Record
 
+import android.icu.util.Calendar
 import android.os.Bundle
-import android.text.style.ClickableSpan
-import android.util.Log
+import android.util.SparseBooleanArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.saloris.R
 import com.example.saloris.databinding.FragmentRecordBinding
@@ -16,6 +15,12 @@ import com.example.saloris.databinding.FragmentRecordBinding
 class RecordFragment : Fragment() {
     private var _binding: FragmentRecordBinding? = null
     private val binding get() = _binding!!
+
+    // Item의 클릭 상태를 저장할 array 객체
+    private val selectedItems = SparseBooleanArray()
+
+    // 직전에 클릭됐던 Item의 position
+    private val prePosition = -1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,17 +37,38 @@ class RecordFragment : Fragment() {
 
         // clickevent로 수정 필요!
         binding.dateChoiceBtn.setOnClickListener {
+
             var datePicker = R.id.date_picker
-            if (it.isClickable)
+
+            if (selectedItems.get(prePosition))
             {
+                selectedItems.delete(prePosition)
                 binding.dateChoiceBtn.setImageResource(R.drawable.ic_outline_arrow_circle_up_24)
-                binding.datePicker.setVisibility(View.VISIBLE);
-            }
-            else if(it.isContextClickable)
-            {
+                binding.datePicker.setVisibility(View.VISIBLE)
+                binding.dateConfirmBtn.setVisibility(View.VISIBLE)
+            } else {
+                selectedItems.delete(prePosition)
+                selectedItems.put(prePosition,true)
+                binding.dateChoiceBtn.setImageResource(R.drawable.ic_baseline_arrow_circle_down_24)
                 binding.datePicker.setVisibility(View.INVISIBLE);
+                binding.dateConfirmBtn.setVisibility(View.INVISIBLE);
             }
         }
+
+//        binding.datePicker.setOnClickListener {
+////            datepickerdialog에 표시할 달력
+//            val datepickercalendar = Calendar.getInstance()
+//            val year = datepickercalendar.get(Calendar.YEAR)
+//            val month = datepickercalendar.get(Calendar.MONTH)
+//            val day = datepickercalendar.get(Calendar.DAY_OF_MONTH)
+//            var dateText = R.id.date_text
+//
+//            binding.datePicker.setOnDateChangedListener { datePicker, year, month, dayOfMonth ->
+//                dateText.toString() = "${year}년 ${month+1}월 ${dayOfMonth}일"
+//            }
+//
+//        }
+
 
         return binding.root
     }
