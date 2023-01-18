ff612524afc61ce2c0425e12e964397fa876a68f
song-na-min <minnasong0707@gmail.com>
2023-01-18 17:23:47 +0900

    chart time
    chart에서 실제 기기 시간 출력 오류 해결

--- mobile/src/main/java/com/example/saloris/data/TimeAxisValueFormat.kt
+++ mobile/src/main/java/com/example/saloris/data/TimeAxisValueFormat.kt
@@ -6,12 +6,12 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
-
+//x축의 값을 시 : 분 형식으로 바꿔서 출력하도록
 class TimeAxisValueFormat :IndexAxisValueFormatter(){
     override fun getFormattedValue(value: Float): String {
         val valueToMinutes = TimeUnit.MINUTES.toMillis(value.toLong())
         var timeMimutes = Date(valueToMinutes)
-        var formatMinutes = SimpleDateFormat("HH:mm")
+        var formatMinutes = SimpleDateFormat("HH:mm") // 시 : 분 형식
 
         return formatMinutes.format(timeMimutes)
     }
