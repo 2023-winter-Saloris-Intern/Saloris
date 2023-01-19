f762c1246f4d4db0a2587096a8ce5a032da5a5a3
song-na-min <minnasong0707@gmail.com>
2023-01-19 13:10:54 +0900

    mobile/data 주석 추가

--- mobile/src/main/java/com/example/saloris/data/showData.kt
+++ mobile/src/main/java/com/example/saloris/data/showData.kt
@@ -49,7 +49,7 @@ class showData : AppCompatActivity() {
         auth = Firebase.auth
         setContentView(R.layout.activity_show_data)
 
-        //DatePicker
+        //DatePicker - 캘린더
         val dayText : TextView = findViewById(R.id.day_text)
         val datePicker : DatePicker =findViewById(R.id.datePicker)
         val iYear : Int = datePicker.year
@@ -63,9 +63,8 @@ class showData : AppCompatActivity() {
         val setDateButton =findViewById<Button>(R.id.setDateButton)
         val button = findViewById<Button>(R.id.button)
         val text = findViewById<TextView>(R.id.textView)
-        //TODO : 1. chart
+
         val TAG = this.javaClass.simpleName
-        //lateinit var lineChart: LineChart
         val heartRateArr = java.util.ArrayList<HeartRate>()
         //chart
         chart = findViewById<View>(R.id.lineChart) as LineChart
@@ -82,8 +81,6 @@ class showData : AppCompatActivity() {
         chart!!.animateXY(1000, 1000)
         chart!!.invalidate()
         val data = LineData()
-
-
         chart!!.data = data
 
         //feedMultiple()
@@ -91,6 +88,7 @@ class showData : AppCompatActivity() {
         // set the marker to the chart
         chart!!.setMarker(mv);
 
+        //캘린더 날짜 선택
         datePicker.setOnDateChangedListener{
                 datePicker,year,month,dayOfMonth->
             dayText.text =  "${year}년 ${month+1}월 ${dayOfMonth}일"
@@ -102,13 +100,14 @@ class showData : AppCompatActivity() {
             //chart!!.visibility =View.VISIBLE
 
         }
+        //다시 캘린더 날짜 선택을 하기 위해 캘린더를 보이게 하는 버튼
         setDateButton.setOnClickListener{
             datePicker.visibility= View.VISIBLE
             //chart!!.visibility =View.GONE
         }
-
+        //선택한 날짜의 데이터를 가져와서 차트로 출력
         button.setOnClickListener{
-            //TODO : colors 초기화
+            //colors 초기화
             colors.clear()
             Log.d("colors",colors.toString())
             last_time=""
@@ -118,12 +117,11 @@ class showData : AppCompatActivity() {
             Log.d("DB start",LocalDateTime.now().toString())
             lifecycleScope.launch(Dispatchers.IO) {
                 val Uid=auth.currentUser?.uid
-//                val Uid = "T"
                 val year=Dyear
                 val month = Dmonth
                 val day =Dday
                 val date = LocalDate.of(year, month, day)
-                //show ( uid , 선택 시간)
+                //show ( uid , 선택 시간) : 선택한 날짜의 데이터를 차트로 출력
                 val show_Data = async {
                     if (Uid != null) {
                         show(Uid,date)
@@ -154,15 +152,17 @@ class showData : AppCompatActivity() {
             val time_min = time.toString().substring(14,16)
             Log.d("time_hour",time_hour)
             Log.d("time_min",time_min)
+            //TODO : time check!!
             val time  = (time_hour.toInt()-9)*60+time_min.toInt()
             Log.d("time",time.toString())
             Log.d("last_time",last_time)
             if(last_time!="" && (last_time.toInt()+1)<time){
+                //처음 시작하는 점이 아니고, 이전 출력시간+1 보다 크면 측정하지 않은 부분이다
                 Log.d("color",last_time + time.toString())
-                Log.d("color","red")
                 //값이 없는 부분 색 삭제
                 colors.add(Color.TRANSPARENT)
             }else if(last_time!=""){
+                //값이 있는 부분은 파란색 선으로
                 colors.add(Color.BLUE)
             }
             last_time=time.toString()
@@ -173,7 +173,7 @@ class showData : AppCompatActivity() {
             chart!!.moveViewToX(time.toFloat()-10f) // 가장 최근 추가한 데이터로 이동
         }
     }
-
+    //chart 설정
     private fun createSet(): LineDataSet {
         val set = LineDataSet(null, "Heart Rate")
         set.fillAlpha = 110
@@ -202,6 +202,7 @@ class showData : AppCompatActivity() {
         var all_string = ""
         Log.d("date",date.javaClass.toString())
         val zoneId = ZoneId.systemDefault()
+        //TODO : DB에서 가져오는 시간 범위 확인하기!
 //        val start = Instant.from(ZonedDateTime.of(date.minusDays(1), LocalTime.of(15,0,0), zoneId))
 //        val stop = Instant.from(ZonedDateTime.of(date, LocalTime.of(14, 59, 59), zoneId))
         val start = Instant.from(ZonedDateTime.of(date, LocalTime.of(0,0,0), zoneId))
@@ -214,6 +215,7 @@ class showData : AppCompatActivity() {
         val token = "yZmCmFFTYYoetepTiOpXDRK8oyL1f_orD6oZH8SXsvlf213z-_iRmXtaf-AjyLe2HS-NhfxcNeY-0K6qR0k6Sw=="
         val client = InfluxDBClientKotlinFactory.create("https://europe-west1-1.gcp.cloud2.influxdata.com", token!!.toCharArray(), org, bucket)
         val heartrate = ArrayList<HeartRate>()
+        //query
         val fluxQuery = ("from(bucket: \"HeartRate\")\n" +
                 "  |> range(start:$start, stop: $stop)\n" +
                 "  |> filter(fn: (r) => r[\"_field\"] == \"HeartRate\")\n" +
@@ -227,12 +229,14 @@ class showData : AppCompatActivity() {
                     print("catch")
                 }
                 .collect {
+                    //하나씩 값 가져와서
                     val heartRate =it.value
                     val  time = it.time?.atZone(zoneId)
                     // : LocalDateTime = LocalDateTime.parse (it.time.toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'")).plusHours(9)
                     Log.d("type of time", time?.javaClass.toString())
                     Log.d("heartRate",time.toString()+"\t"+heartRate.toString())
                     val h_string = "heartRate : "+time.toString()+"\t"+heartRate.toString()+"\n"
+                    //chart 데이터에 저장
                     addEntry(time.toString(),heartRate.toString())
                     //heartrate.add(heartRate)
                     all_string += h_string
