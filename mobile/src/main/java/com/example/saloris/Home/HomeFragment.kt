36e9c125ba8d16c7b2e6ec86f444bd84e14f4cca
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-01-21 19:37:01 +0900

    Feat: check connect

--- mobile/src/main/java/com/example/saloris/Home/HomeFragment.kt
+++ mobile/src/main/java/com/example/saloris/Home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.saloris.Home
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
@@ -9,7 +10,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
@@ -22,11 +25,30 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.example.saloris.util.MakeToast
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.wearable.MessageEvent
+import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.*
+import java.util.HashSet
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), CoroutineScope by MainScope() {
+    private val wearableAppCheckPayload = "AppOpenWearable"
+    private val wearableAppCheckPayloadReturnACK = "AppOpenWearableACK"
+
+    private var currentAckFromWearForAppOpenCheck: String? = null
+    private val APP_OPEN_WEARABLE_PAYLOAD_PATH = "/APP_OPEN_WEARABLE_PAYLOAD"
+
+    private val MESSAGE_ITEM_RECEIVED_PATH: String = "/message-item-received"
+
+    private val TAG_GET_NODES: String = "getnodes1"
+    private val TAG_MESSAGE_RECEIVED: String = "receive1"
+
+    private var messageEvent: MessageEvent? = null
+    private var wearableNodeUri: String? = null
+
     private lateinit var binding: FragmentHomeBinding
     private lateinit var navController: NavController
-
+    private var wearableDeviceConnected: Boolean = false
     private lateinit var onBackPressedCallback: OnBackPressedCallback
     var btnBackPressedTime: Long = 0
 
@@ -81,7 +103,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        wearableDeviceConnected = false
         navController = Navigation.findNavController(view)
 
         /* User Authentication */
@@ -106,6 +128,166 @@ class HomeFragment : Fragment() {
             startActivity(intent)
         }
 
+        binding.check.setOnClickListener {
+            //wearable device가 연결되었는지 확인하는 버튼
+            if (!wearableDeviceConnected) {
+                val tempAct: Activity = requireActivity() as AppCompatActivity
+                //Couroutine
+                initialiseDevicePairing(tempAct)
+            }
+        }
+
+    }
+
+    //wearOS와 연동되었는지 확인
+    @SuppressLint("SetTextI18n")
+    private fun initialiseDevicePairing(tempAct: Activity) {
+        //Coroutine
+        launch(Dispatchers.Default) {
+            var getNodesResBool: BooleanArray? = null
+
+            try {
+                Log.d("try","try get nodes res bool")
+                getNodesResBool =
+                    getNodes(tempAct.applicationContext)
+            } catch (e: Exception) {
+                Log.d("try","try exception")
+                e.printStackTrace()
+            }
+
+            //UI Thread
+            withContext(Dispatchers.Main) {
+                getNodesResBool?.get(1)?.let { Log.d("getnodesresbool : ", it.toString()) }
+                if (getNodesResBool!![0]) {
+                    //if message Acknowlegement Received
+                    if (getNodesResBool[1]) {
+                        //binding.deviceconnectionStatusTv.text =
+                        //    "Wearable device paired and app is open."
+                        //binding.deviceconnectionStatusTv.visibility = View.VISIBLE
+                        wearableDeviceConnected = true
+                        //binding.sendmessageButton.visibility = View.VISIBLE
+                        binding.notConnect.visibility = View.GONE
+                        binding.isConnect.visibility = View.VISIBLE
+                    } else {
+                        //binding.deviceconnectionStatusTv.text =
+                        //    "Wearable device paired but app isn't open."
+                        //binding.deviceconnectionStatusTv.visibility = View.VISIBLE
+                        wearableDeviceConnected = false
+                        //binding.sendmessageButton.visibility = View.GONE
+                        binding.notConnect.visibility = View.GONE
+                        binding.isConnect.visibility = View.VISIBLE
+                    }
+                } else {
+                    //binding.deviceconnectionStatusTv.text =
+                    //    "Wearable device not paired and connected."
+                    //binding.deviceconnectionStatusTv.visibility = View.VISIBLE
+                    wearableDeviceConnected = false
+                    //binding.sendmessageButton.visibility = View.GONE
+                    binding.notConnect.visibility = View.VISIBLE
+                    binding.isConnect.visibility = View.GONE
+                }
+            }
+        }
+    }
+    //현재 모바일이랑 연동된 워치의 node를 가져와서 확인
+    private fun getNodes(context: Context): BooleanArray {
+        val nodeResults = HashSet<String>()
+        val resBool = BooleanArray(2)
+        resBool[0] = false //nodePresent : true이면 연결되어있다
+        resBool[1] = false //wearableReturnAckReceived : true이면 워치에 앱이 열려있다
+        val nodeListTask =
+            Wearable.getNodeClient(context).connectedNodes
+        try {
+            // Block on a task and get the result synchronously (because this is on a background thread).
+            val nodes =
+                Tasks.await(
+                    nodeListTask
+                )
+            //Log.e(TAG_GET_NODES, "Task fetched nodes")
+            for (node in nodes) {
+                //Log.e(TAG_GET_NODES, "inside loop")
+                nodeResults.add(node.id)
+                try {
+                    val nodeId = node.id
+                    // Set the data of the message to be the bytes of the Uri.
+                    val payload: ByteArray = wearableAppCheckPayload.toByteArray()
+                    // Send the rpc
+                    // Instantiates clients without member variables, as clients are inexpensive to
+                    // create. (They are cached and shared between GoogleApi instances.)
+                    val sendMessageTask =
+                        Wearable.getMessageClient(context)
+                            .sendMessage(nodeId, APP_OPEN_WEARABLE_PAYLOAD_PATH, payload)
+                    Log.d("sendMessageToWearable","send message")
+                    //워치로 메시지를 보내고 5번동안 받으면 앱이 열려있고 안받으면 앱이 없다
+                    try {
+                        // Block on a task and get the result synchronously (because this is on a background thread).
+                        val result = Tasks.await(sendMessageTask)
+                        Log.d(TAG_GET_NODES, "send message result : $result")
+                        resBool[0] = true
+                        //Wait for 1000 ms/1 sec for the acknowledgement message
+                        //Wait 1
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(100)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep 1")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Wait 2
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(150)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep 2")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Wait 3
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(200)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep 3")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Wait 4
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(250)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep 4")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Wait 5
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(350)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep 5")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        resBool[1] = false
+                        Log.d(
+                            TAG_GET_NODES,
+                            "ACK thread timeout, no message received from the wearable "
+                        )
+                    } catch (exception: Exception) {
+                        exception.printStackTrace()
+                    }
+                } catch (e1: Exception) {
+                    Log.d(TAG_GET_NODES, "send message exception")
+                    e1.printStackTrace()
+                }
+            } //end of for loop
+        } catch (exception: Exception) {
+            Log.e(TAG_GET_NODES, "Task failed: $exception")
+            exception.printStackTrace()
+        }
+        return resBool
     }
 
     override fun onDestroyView() {
