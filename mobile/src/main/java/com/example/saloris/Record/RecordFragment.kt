3625b4ccb3627420fe15051aed02a64c6d54b116
HeewonP825 <7hwon@naver.com>
2023-01-30 12:41:34 +0900

    design recordFragment

--- mobile/src/main/java/com/example/saloris/Record/RecordFragment.kt
+++ mobile/src/main/java/com/example/saloris/Record/RecordFragment.kt
@@ -28,7 +28,6 @@ import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.toTimeUnit
 
-
 //line chart 시도
 class TimeAxisValueFormat : IndexAxisValueFormatter() {
 
@@ -43,11 +42,6 @@ class TimeAxisValueFormat : IndexAxisValueFormatter() {
     }
 }
 
-//data class ChartData(
-//    var lableData: String = "",
-//    var valData: Double = 0.0
-//)
-
 class RecordFragment : Fragment() {
     private var _binding: FragmentRecordBinding? = null
     private val binding get() = _binding!!
@@ -56,10 +50,6 @@ class RecordFragment : Fragment() {
     private var lineDataSet = ArrayList<ILineDataSet>() // 데이터배열 -> 데이터 셋
     private var lineData: LineData = LineData()
     lateinit var chart: LineChart
-
-    //private var chart: LineChart? = null
-
-    //private var lineChart: LineChart? = null
 
     // Item의 클릭 상태를 저장할 array 객체
     private val selectedItems = SparseBooleanArray()
@@ -73,58 +63,6 @@ class RecordFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentRecordBinding.inflate(inflater, container, false)
-
-        //val dateChoiceBtn: ImageButton = requireView().findViewById(com.example.saloris.R.id.date_choice_btn)
-
-//        binding.deviceScanBtn.setOnClickListener {
-//            loadFragment(ScanFragment())
-//        }
-
-//        val view = inflater.inflate(R.layout.fragment_record, container, false)
-//        val button: Button = view.findViewById(R.id.date_confirm_btn)
-//        val dateTextView: TextView = view.findViewById(R.id.date_text)
-//        val datePicker: DatePicker = view.findViewById(R.id.date_picker)
-//
-//        val iYear: Int = datePicker.year
-//        val iMonth: Int = datePicker.month+1
-//        val iDay: Int = datePicker.dayOfMonth
-
-//        dateTextView.text = "${iYear}년 ${iMonth}월 ${iDay}일"
-
-//        datePicker.setOnDateChangedListener { datePicker, year, month, dayOfMonth ->
-//            dateTextView.text = "${year}년 ${month+1}월 ${dayOfMonth}일"
-//        }
-//        button.setOnClickListener { // 버튼 click 시 선택 된 날짜 정보 불러옴
-//            dateTextView.setText("Selected Date: "+ datePicker.getDayOfMonth()+"/"+ (datePicker.getMonth() + 1)+"/"+datePicker.getYear());
-//        }
-
-        // clickevent로 수정 필요!
-//        binding.dateChoiceBtn.setOnClickListener {
-//
-////          val datepickerHeaderid = binding.datePicker.getChildAt(0)
-////              .resources.getIdentifier("date_picker_header", "id", "android")
-////          binding.datePicker.findViewById<View>(datepickerHeaderid).visibility = View.GONE
-//
-//            val cal = java.util.Calendar.getInstance()
-//            // 사용자가 OK버튼을 누르면 바인딩된 text가 바뀜
-//            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, day ->
-//                binding.dateText.text = "${year}년 ${month + 1}월 ${day}일"
-//            }
-//            // 다이얼로그 생성, 다이얼로그를 띄울때 오늘 날짜를 기본 데이터로 잡아준다.
-//            //DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
-//            //스피너 모드로 바꿔주기 deprecated 됐기 때문에 themes 에서 추가해줘야함
-//            val dateDialog = DatePickerDialog(
-//                requireActivity(), dateSetListener, cal.get(java.util.Calendar.YEAR), cal.get(
-//                    java.util.Calendar.MONTH
-//                ), cal.get(java.util.Calendar.DAY_OF_MONTH)
-//            )
-//                .apply {
-//                    datePicker.maxDate = System.currentTimeMillis()
-//                }
-//
-//            dateDialog.show()
-//            //dateDialog.datePicker.spinnersShown = true
-//        }
 
         chart =  binding.dayChart
 
@@ -155,6 +93,7 @@ class RecordFragment : Fragment() {
                 binding.dateChoiceBtn.setImageResource(R.drawable.ic_baseline_keyboard_arrow_up_24)
                 binding.calendarView1.setVisibility(View.VISIBLE)
                 binding.dateConfirmBtn.setVisibility(View.VISIBLE)
+                binding.graphBtn.setVisibility(View.GONE)
                 binding.dayChart.setVisibility(View.GONE)
                 binding.heartRateNum.setVisibility(View.GONE)
 
@@ -162,6 +101,7 @@ class RecordFragment : Fragment() {
                     binding.dateConfirmBtn.setVisibility(View.GONE)
                     binding.calendarView1.setVisibility(View.GONE)
                     binding.dateChoiceBtn.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24)
+                    binding.graphBtn.setVisibility(View.VISIBLE)
                     binding.dayChart.setVisibility(View.VISIBLE)
                     binding.heartRateNum.setVisibility(View.VISIBLE)
                 }
@@ -182,71 +122,6 @@ class RecordFragment : Fragment() {
             }
 
         }
-
-//        chart = findFragmentById(com.example.saloris.R.id.day_chart)
-//
-//        val values: ArrayList<Map.Entry<*, *>> = ArrayList()
-//
-//        for (i in 0..9) {
-//            val `val` = (Math.random() * 10).toFloat()
-//            values.add(MutableMap.MutableEntry<Any?, Any?>(i, `val`))
-//        }
-//
-//        val set1: LineDataSet
-//        set1 = LineDataSet(values, "DataSet 1")
-//
-//        val dataSets: ArrayList<ILineDataSet> = ArrayList()
-//        dataSets.add(set1) // add the data sets
-//
-//
-//        // create a data object with the data sets
-//
-//        // create a data object with the data sets
-//        val data = LineData(dataSets)
-//
-//        // black lines and points
-//
-//        // black lines and points
-//        set1.color = Color.BLACK
-//        set1.setCircleColor(Color.BLACK)
-//
-//        // set data
-//
-//        // set data
-//        chart.setData(data)
-
-//        그래프 출력
-//        chart = view?.findViewById(R.id.day_chart) as LineChart
-//
-//        chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-//
-//        chart.axisRight.isEnabled = false
-//        chart.legend.textColor = Color.WHITE
-//        chart.animateXY(2000, 2000)
-//        chart.invalidate()
-//
-//        val data = LineData()
-//        chart.data = data
-
-
-//        binding.datePicker.setOnClickListener(){
-//            val cal = java.util.Calendar.getInstance()
-//            // 사용자가 OK버튼을 누르면 바인딩된 text가 바뀜
-//            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, day ->
-//                binding.dateText.text = "${year}년. ${month+1}월. ${day}일"
-//            }
-//            // 다이얼로그 생성, 다이얼로그를 띄울때 오늘 날짜를 기본 데이터로 잡아준다.
-//            //DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
-//            //스피너 모드로 바꿔주기 deprecated 됐기 때문에 themes 에서 추가해줘야함
-//            val dateDialog = DatePickerDialog(requireActivity(), dateSetListener, cal.get(java.util.Calendar.YEAR), cal.get(
-//                java.util.Calendar.MONTH), cal.get(java.util.Calendar.DAY_OF_MONTH))
-//            dateDialog.show()
-//            dateDialog.datePicker.spinnersShown = true
-//
-//        }
-
-//        view?.let { setChartView(it) }
-
 
         return binding.root
     }
