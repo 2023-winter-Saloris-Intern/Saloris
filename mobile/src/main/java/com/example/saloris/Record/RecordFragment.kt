eaecb40a2a60db16d9f3da0d21cf81ce60781cf0
HeewonP825 <7hwon@naver.com>
2023-02-02 16:37:43 +0900

    fix recordFragment

--- mobile/src/main/java/com/example/saloris/Record/RecordFragment.kt
+++ mobile/src/main/java/com/example/saloris/Record/RecordFragment.kt
@@ -51,21 +51,6 @@ import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.round
 
-
-////line chart 시도
-//class TimeAxisValueFormat : IndexAxisValueFormatter() {
-//
-//    @OptIn(ExperimentalTime::class)
-//    override fun getFormattedValue(value: Float): String {
-//        //Float(min) -> date
-//        var valueToMinutes = DurationUnit.MINUTES.toTimeUnit().toMillis(value.toLong())
-//        var timeMinutes = Date(valueToMinutes)
-//        var formatMinutes = SimpleDateFormat("HH:mm")
-//
-//        return formatMinutes.format(timeMinutes)
-//    }
-//}
-
 class RecordFragment : Fragment() {
     private var _binding: FragmentRecordBinding? = null
     private val binding get() = _binding!!
@@ -120,10 +105,6 @@ class RecordFragment : Fragment() {
 
         chart =  binding.dayChart
 
-//        prepareChartData(lineData, chart)
-//        initChartData()
-//        initChart()
-
         binding.calendarView1.setVisibility(View.GONE)
         binding.dayChart.setVisibility(View.GONE)
         binding.heartRateNum.setVisibility(View.GONE)
@@ -133,6 +114,7 @@ class RecordFragment : Fragment() {
 
             val dateText: TextView = requireView().findViewById(R.id.date_text)
             val calendarView: CalendarView = requireView().findViewById(R.id.calendar_view1)
+//            val calendarView: MaterialCalendarView = requireView().findViewById(R.id.calendar_view1)
 
             val dateFormat: DateFormat = SimpleDateFormat("yyyy년MM월dd일")
 
@@ -142,6 +124,7 @@ class RecordFragment : Fragment() {
 //                .setMinimumDate(CalendarDay.from(Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH))
 
             calendarView.maxDate = System.currentTimeMillis()
+
             DayArr.clear()
             averageArr.clear()
             lifecycleScope.launch(Dispatchers.IO) {
@@ -550,77 +533,6 @@ class RecordFragment : Fragment() {
         //print(heartrate)
         return all_string
     }
-
-//    //    Line Chart 시도
-//    private fun initChartData() {
-//        //더미데이터
-//        chartData.add(Entry(-240f, 0f))
-//        chartData.add(Entry(-200f, 30f))
-//        chartData.add(Entry(-50f, 100f))
-//        chartData.add(Entry(-120f, 20f))
-//        chartData.add(Entry((1200).toFloat(), 0f))
-//
-//        var set = LineDataSet(chartData, "set1")
-//        lineDataSet.add(set)
-//        lineData = LineData(lineDataSet)
-//
-//        set.lineWidth = 2F
-//        set.setDrawValues(false)
-//        set.highLightColor = Color.TRANSPARENT
-//        set.mode = LineDataSet.Mode.STEPPED
-//
-//    }
-
-//    private fun initChart() {
-//        chart.run {
-//            setDrawGridBackground(false)
-//            setBackgroundColor(Color.WHITE)
-//            legend.isEnabled = false
-//        }
-//
-//        val xAxis = chart.xAxis
-//        xAxis.setDrawLabels(true) // label 표시 여부
-//        xAxis.axisMaximum = 1200f // 60min * 24hour
-//        xAxis.axisMinimum = -240f
-//        xAxis.labelCount = 5
-//        xAxis.valueFormatter = TimeAxisValueFormat()
-//
-//        xAxis.textColor = Color.BLACK
-//        xAxis.position = XAxis.XAxisPosition.BOTTOM // x축 라벨 위치
-//        xAxis.setDrawLabels(true) // gridLine 표시
-//        xAxis.setDrawAxisLine(true) // AxisLine 표시
-//
-//        // 왼쪽 y축 값
-//        val yLAxis = chart.axisLeft
-//        yLAxis.axisMaximum = 4.5f // y축 최대값
-//        yLAxis.axisMinimum = -0.5f // y축 최소값
-//
-//        // 왼쪽 y축 도메인 변경
-//        val yAxisVals = ArrayList<String>(Arrays.asList("50", "60", "70", "80", "90", "100"))
-//        yLAxis.valueFormatter = IndexAxisValueFormatter(yAxisVals)
-//        yLAxis.granularity = 1f
-//
-//        // 오른쪽 y축 값
-//        val yRAxix = chart.axisRight
-//        yRAxix.setDrawLabels(false)
-//        yRAxix.setDrawAxisLine(false)
-//        yRAxix.setDrawGridLines(false)
-//
-//        // 마커 설정
-////        val marker = MarkerView(requireContext(), com.example.saloris.R.drawable.graph_marker)
-////        marker.chartView = chart
-////        chart.marker = marker
-//
-//        chart!!.description.isEnabled = false // 설명
-//        chart!!.data = lineData // 데이터 설정
-//
-//        chart!!.invalidate() // 다시 그리기
-//    }
-//
-//    private fun prepareChartData(data: LineData, lineChart: LineChart) {
-//        lineChart.data = data // LineData 전달
-//        lineChart.invalidate() // LineChart 갱신해 데이터 표시
-//    }
 }
 
 class EventDecorator() : DayViewDecorator {
