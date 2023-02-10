cc1f27ec50b9e5eb1f2c795d56a9e5481eaf3111
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-02-10 15:17:39 +0900

    Update RequiredInfo3Fragment.kt

--- mobile/src/main/java/com/example/saloris/RequiredInfo/RequiredInfo3Fragment.kt
+++ mobile/src/main/java/com/example/saloris/RequiredInfo/RequiredInfo3Fragment.kt
@@ -27,6 +27,7 @@ class RequiredInfo3Fragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
     private lateinit var storage: FirebaseStorage
+    var userInfo = RequiredInfo()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,44 +46,23 @@ class RequiredInfo3Fragment : Fragment() {
         //Initialize Firebase Storage
         storage = FirebaseStorage.getInstance()
         auth = FirebaseAuth.getInstance()
-        var userInfo = RequiredInfo()
-
-        var cardColor = ContextCompat.getDrawable(requireContext(),R.drawable.blue_round_button)
-        var textColor = ContextCompat.getColor(requireContext(),R.color.white)
-        var originalCardColor = ContextCompat.getDrawable(requireContext(),R.drawable.light_grey_btn)
-        var originalTextColor = ContextCompat.getColor(requireContext(),R.color.grey)
-
-        val numberPicker: NumberPicker = binding.numberPicker
-        numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
-            if (binding.numberPicker != null) {
-                // 값이 null이 아닐 때, 다음 페이지로 이동 가능
-                binding.goNextStepBtn.setOnClickListener {
-                    //navController.navigate(R.id.action_requiredInfoFragment_to_requiredInfo1Fragment)
-                    binding.goNextStepBtn.setBackgroundDrawable(cardColor)
-                    binding.goNextStepBtn.setTextColor(textColor)
-                }
-            } else {
-                // 값이 null일 때, 다음 페이지로 이동 불가
-                binding.goNextStepBtn.isEnabled = false
-                binding.goNextStepBtn.setBackgroundDrawable(originalCardColor)
-                binding.goNextStepBtn.setTextColor(originalTextColor)
-            }
-        }
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var originalCardColor = ContextCompat.getDrawable(requireContext(),R.drawable.light_grey_btn)
+        var originalTextColor = ContextCompat.getColor(requireContext(),R.color.grey)
         navController = Navigation.findNavController(view)
-
+        binding.goNextStepBtn.isEnabled = false
+        binding.goNextStepBtn.setBackgroundDrawable(originalCardColor)
+        binding.goNextStepBtn.setTextColor(originalTextColor)
         binding.goNextStepBtn.setOnClickListener {
-//            navController.navigate(R.id.action_requiredInfo2Fragment_to_requiredInfo3Fragment)
-
-            if (binding.numberPicker != null) {
-                navController.navigate(R.id.action_requiredInfo2Fragment_to_requiredInfo3Fragment)
-
-            }
+            navController.navigate(R.id.action_requiredInfo2Fragment_to_requiredInfo3Fragment)
+//            if (binding.numberPicker != null) {
+//                navController.navigate(R.id.action_requiredInfo2Fragment_to_requiredInfo3Fragment)
+//            }
         }
 
         binding.goBackBtn.setOnClickListener {
@@ -98,9 +78,16 @@ class RequiredInfo3Fragment : Fragment() {
         numberPicker.value = 50 // 초기값
         // 값이 변경될 때 마다 Firestore에 값을 업데이트
         numberPicker.setOnValueChangedListener { _, _, newVal ->
-            val userInfo = RequiredInfo()
+            changeButton()
             userInfo.userWeight = newVal.toString()
             firestore?.collection("users")?.document(auth?.uid!!)?.update("userWeight", userInfo.userWeight)
         }
+    }
+    private fun changeButton(){
+        var cardColor = ContextCompat.getDrawable(requireContext(),R.drawable.blue_round_button)
+        var textColor = ContextCompat.getColor(requireContext(),R.color.white)
+        binding.goNextStepBtn.isEnabled = true
+        binding.goNextStepBtn.setBackgroundDrawable(cardColor)
+        binding.goNextStepBtn.setTextColor(textColor)
     }
 }
